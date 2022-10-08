2853b3ac2d04cf056503236334d276197549e73c
Harrynoqs <109760801+Harrynoqs@users.noreply.github.com>
2022-10-08 17:44:55 +0300

    Edit js

--- src/modules/Todolist.js
+++ src/modules/Todolist.js
@@ -21,7 +21,7 @@ class ToDoChore {
         this.refreshTasks();
       }
 
-      addTask(description) {
+      addchore(description) {
         const task = {
           description,
           completed: false,
@@ -33,7 +33,7 @@ class ToDoChore {
       }
 
       // editing of task
-      changeComplete(i) {
+      editComplete(i) {
         const status = this.choreArray[i - 1].completed;
         this.choreArray[i - 1] = {
           ...this.choreArray[i - 1],
